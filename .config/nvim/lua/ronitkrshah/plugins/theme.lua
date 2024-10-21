6def772d2ebdbbdb0625db431a7a730148085e62
RKS <ronitkrshah@tuta.io>
2024-10-21 16:05:15 +0530

    Update Neovim theme

--- .config/nvim/lua/ronitkrshah/plugins/theme.lua
+++ .config/nvim/lua/ronitkrshah/plugins/theme.lua
@@ -1,12 +1,24 @@
 return {
-  "tiagovla/tokyodark.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false,
   priority = 1000,
   config = function()
-    require("tokyodark").setup({
-      gamma = 1.4,
+    require("catppuccin").setup({
+      flavour = "frappe",
+      background = {
+        light = "latte",
+        dark = "frappe",
+      },
+      styles = {
+        comments = { "bold", "italic" },
+        conditionals = { "bold", "italic" },
+        functions = { "bold", "italic" },
+        booleans = { "bold", "italic" },
+        types = { "bold", "italic" },
+      },
     })
 
-    vim.cmd.colorscheme("tokyodark")
+    vim.cmd.colorscheme("catppuccin")
   end
 }
